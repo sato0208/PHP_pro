80c7d2760c6db9d1bbd99ccb84e5261427c74562
sato0208 <kujira028@gmail.com>
2020-08-17 20:54:12 +0900

    フォームの入力に問題のある場合とない場合の表示を分岐させる

--- staff/staff_add_check.php
+++ staff/staff_add_check.php
@@ -32,6 +32,23 @@
       {
         print 'パスワードが一致しません <br/>';
       }
+      if($staff_name==''||$staff_pass==''||$staff_pass!=$staff_pass2)
+      {
+        print '<form>';
+        print '<input type="button" onclick="history.back()" value="戻る">';
+        print '</form>';
+      }
+      else
+      {
+        $staff_pass=md5($staff_pass);
+        print '<form method="post" action="staff_add_done.php">';
+        print '<input type="hidden" name="name" value="'.$staff_name.'">';
+        print '<input type="hidden" name="pass" value="'.$staff_pass.'">';
+        print '<br/>';
+        print '<input type="button" onclick="histry.back()" value="戻る">';
+        print '<input type="submit" value="OK">';
+        print '</form>';
+      }
     ?>
   </body>
 </html>
