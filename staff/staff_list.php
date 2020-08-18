d0f6b51ce30e982810efccac330550671a88cb7a
sato0208 <kujira028@gmail.com>
2020-08-19 03:20:08 +0900

    データベースへの接続

--- staff/staff_list.php
+++ staff/staff_list.php
@@ -7,7 +7,12 @@
   <body>
     try
     {
-
+      // データベースに接続
+        $dsn ='mysql:dbname=shop;host=localhost;charset=utf8';
+        $user ='root';
+        $password ='';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
   </body>
 </html>
