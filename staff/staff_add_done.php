8153ff61ca8206f257a487283d033fba9dfa8c2f
sato0208 <kujira028@gmail.com>
2020-08-18 14:36:31 +0900

    データベースサーバーの障害対策

--- staff/staff_add_done.php
+++ staff/staff_add_done.php
@@ -8,35 +8,43 @@
     <?php
       try
       {
+        // 前画面から受け取ったデータをコピー
         $staff_name = $_POST['name'];
         $staff_pass = $_POST['pass'];
 
+        // 入力データに安全対策
         $staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
         $staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
 
+        // データベースに接続
         $dsn ='mysql:dbname=shop;host=localhost;charset=utf8';
         $user ='root';
         $password ='';
         $dbh = new PDO($dsn, $user, $password);
         $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        // sql文を使ってコードを追加
         $sql = 'INSERT INTO mst_staff(name,password)VALUES (?,?)';
         $stmt = $dbh ->prepare($sql);
         $data[] = $staff_name;
         $data[] = $staff_pass;
         $stmt->execute($data);
 
+        // データベースから切断
         $dbh = null;
 
+        // さんを追加しましたと表示させる
         print $staff_name;
         print 'さんを追加しました。<br/>';
       }
       catch (Exception $e)
       {
+        // データベースサーバーに障害が生じた際このプログラムが動く
         print 'ただいま障害により大変ご迷惑をおかけしています';
         exit();
       }
     ?>
+    <!-- TODO スタッフ一覧画面の作成 -->
     <a href="staff_list.php">戻る</a>
   </body>
 </html>
