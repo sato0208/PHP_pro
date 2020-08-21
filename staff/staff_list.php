1a9880807111916c444b2524ae436588a9a2b4c3
sato0208 <kujira028@gmail.com>
2020-08-21 15:24:37 +0900

    [update]スタッフをひとり選べるようにする

--- staff/staff_list.php
+++ staff/staff_list.php
@@ -8,7 +8,7 @@
   <?php
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
-?> 
+?>
     <?php
     try
     {
@@ -19,21 +19,28 @@ error_reporting(E_ALL);
         $dbh = new PDO($dsn, $user, $password);
         $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql ='SELECT name FROM mst_staff WHERE 1';
+        $sql ='SELECT code, name FROM mst_staff WHERE 1';
         $stmt = $dbh -> prepare($sql);
         $stmt -> execute();
 
         $dbh = null;
 
         print 'スタッフ一覧 <br/><br/>';
+
+        // 修正画面屁飛べるようにする
+        print '<form method="post" action="staff_edit.php">';
         while(true){
           $rec = $stmt->fetch(PDO::FETCH_ASSOC);
           if($rec == false){
             break;
           }
+          // ラジオボタン でどのスタッフを選んだかを跳び先でわかるようにスタッフコードを渡す
+          print '<input type="radio" name="staffcode" value="'.$rec['code'].'">';
           print $rec['name'];
           print '<br/>';
         }
+        print '<input type="submit" value="修正">';
+        print '</form>';
     }
     catch(Exception $e){
       print 'ただいま障害により大変ご迷惑をおかけしております';
