940e2e1cbcc559da56355278a0d0c57dc54dd2df
sato0208 <kujira028@gmail.com>
2020-08-21 15:46:16 +0900

    [update]修正用のフォーム作成

--- staff/staff_edit.php
+++ staff/staff_edit.php
@@ -34,5 +34,22 @@
       exit();
     }
     ?>
+
+    スタッフ修正<br/>
+    <br/>
+    <br/>
+    <form method="post" action="staff_edit_check.php">
+      <input type="hidden" name="code" value="<?php print $staff_code; ?>">
+      スタッフ名<br/>
+      <!-- 名前を入れた状態で表示させる -->
+      <input type="text" name="name" style="width:200px" value="<?php print $staff_name; ?>"><br/>
+      パスワードを入力して下さい。<br/>
+      <input type="password" name="pass" style="width:100px"><br/>
+      パスワードをもう一度入力してください。<br/>
+      <input type="password" name="pass2" style="width:200px"><br/>
+      <br/>
+      <input type="button" onclick="history.back()" value="戻る">
+      <input type="submit" value="OK">
+    </form>
   </body>
 </html>
