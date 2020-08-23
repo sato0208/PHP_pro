d6fae5864dea41e393deceedddd424c7cb8e0fcb
sato0208 <kujira028@gmail.com>
2020-08-23 17:22:11 +0900

    [update]スタッフ情報参照ページへ飛ばす

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -1,8 +1,24 @@
 <?php
+
+// スタッフ参照ページへ飛ぶ
+if(isset($_POST['disp']) == true){
+  // もしスタッフコードが選ばれていなかったらNG画面に飛ぶ
+  if(isset($_POST['staffcode']) == false){
+    header('Location: staff_ng.php');
+    exit();
+  }
+  $staff_code=$_POST['staffcode'];
+  header('Location: staff_disp.php?staffcode='.$staff_code);
+  exit();
+}
+
+// スタッフ追加ページへ飛ぶ
 if(isset($_POST['add']) == true){
   header('Location: staff_add.php');
   exit();
 }
+
+// スタッフ編集ページへ飛ぶ
 if(isset($_POST['edit']) == true){
   // もしスタッフコードが選ばれていない場合はNG画面に飛ぶ
   if(isset($_POST['staffcode']) == false){
@@ -13,6 +29,8 @@ if(isset($_POST['edit']) == true){
   header('Location: staff_edit.php?staffcode='.$staff_code);
   exit();
 }
+
+// スタッフ削除ページへ飛ぶ
 if(isset($_POST['delete']) == true){
   // もしスタッフコードが選ばれていない場合はNG画面に飛ぶ
   if(isset($_POST['staffcode']) == false){
