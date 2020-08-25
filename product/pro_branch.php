b9403c5610026340fb53e2f3569b4537c4851268
sato0208 <kujira028@gmail.com>
2020-08-25 19:17:49 +0900

    [add]商品リストからの分岐実装

---
+++ product/pro_branch.php
@@ -0,0 +1,44 @@
+<?php
+
+// スタッフ参照ページへ飛ぶ
+if(isset($_POST['disp']) == true){
+  // もしスタッフコードが選ばれていなかったらNG画面に飛ぶ
+  if(isset($_POST['procode']) == false){
+    header('Location: pro_ng.php');
+    exit();
+  }
+  $pro_code=$_POST['procode'];
+  header('Location: pro_disp.php?staffcode='.$pro_code);
+  exit();
+}
+
+// スタッフ追加ページへ飛ぶ
+if(isset($_POST['add']) == true){
+  header('Location: pro_add.php');
+  exit();
+}
+
+// スタッフ編集ページへ飛ぶ
+if(isset($_POST['edit']) == true){
+  // もしスタッフコードが選ばれていない場合はNG画面に飛ぶ
+  if(isset($_POST['procode']) == false){
+    header('Location: pro_ng.php');
+    exit();
+  }
+  $pro_code=$_POST['procode'];
+  header('Location: pro_edit.php?procode='.$pro_code);
+  exit();
+}
+
+// スタッフ削除ページへ飛ぶ
+if(isset($_POST['delete']) == true){
+  // もしスタッフコードが選ばれていない場合はNG画面に飛ぶ
+  if(isset($_POST['procode']) == false){
+    header('Location: pro_ng.php');
+    exit();
+  }
+  $pro_code=$_POST['procode'];
+  header('Location: pro_delete.php?procode='.$pro_code);
+  exit();
+}
+?>
