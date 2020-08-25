52d44e9fd4bf2fc9bd1a180e94bad8d743e5f45f
sato0208 <kujira028@gmail.com>
2020-08-25 19:54:46 +0900

    [add]商品の修正実行機能実装

---
+++ product/pro_edit_done.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>ろくまる農園</title>
+  </head>
+  <body>
+    <?php
+      try
+      {
+        // 前画面から受け取ったデータをコピー
+        $pro_code = $_POST['code'];
+        $pro_name = $_POST['name'];
+        $pro_price = $_POST['price'];
+
+        // 入力データに安全対策
+        $pro_code = htmlspecialchars($pro_code,ENT_QUOTES,'UTF-8');
+        $pro_name = htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+        $pro_price = htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+
+        // データベースに接続
+        $dsn ='mysql:dbname=shop;host=localhost;charset=utf8';
+        $user ='root';
+        $password ='root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // sql文を使ってコードを追加
+        $sql = 'UPDATE mst_product SET name=?,price=? WHERE code=?';
+        $stmt = $dbh ->prepare($sql);
+        $data[] = $pro_name;
+        $data[] = $pro_price;
+        $data[] = $pro_code;
+        $stmt->execute($data);
+
+        // データベースから切断
+        $dbh = null;
+
+        // さんを追加しましたと表示させる
+        print '修正しました。<br/>';
+      }
+      catch (Exception $e)
+      {
+        // データベースサーバーに障害が生じた際このプログラムが動く
+        print 'ただいま障害により大変ご迷惑をおかけしています';
+        exit();
+      }
+    ?>
+    <!-- TODO スタッフ一覧画面の作成 -->
+    <a href="pro_list.php">戻る</a>
+  </body>
+</html>
