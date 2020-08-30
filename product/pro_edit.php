c9e40be36c814e895a8ed6cb7c5dc3f9d5238b06
sato0208 <kujira028@gmail.com>
2020-08-30 15:45:42 +0900

    [update]商品修正画面で画像を表示

--- product/pro_edit.php
+++ product/pro_edit.php
@@ -22,7 +22,7 @@ error_reporting(E_ALL);
       $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
       // スタッフコードで絞り込む
-      $sql = 'SELECT name,price FROM mst_product WHERE code=?';
+      $sql = 'SELECT name,price,gazou FROM mst_product WHERE code=?';
       $stmt = $dbh ->prepare($sql);
       $data[]=$pro_code;
       $stmt->execute($data);
@@ -31,8 +31,15 @@ error_reporting(E_ALL);
       $rec = $stmt->fetch(PDO::FETCH_ASSOC);
       $pro_name=$rec['name'];
       $pro_price=$rec['price'];
+      $pro_gazou_name_ord=$rec['gazou'];
 
       $dbh = null;
+
+      if($pro_gazou_name_ord==''){
+        $disp_gazou='';
+      }else{
+        $disp_gazou='<img src="../gazou/'.$pro_gazou_name_ord.'">';
+      }
     }
     catch(Exception $e){
       print 'ただいま障害により大変ご迷惑をおかけしております';
@@ -45,13 +52,19 @@ error_reporting(E_ALL);
     商品コード<br/>
     <?php print $pro_code; ?>
     <br/>
-    <form method="post" action="pro_edit_check.php">
+    <form method="post" action="pro_edit_check.php" enctype="multipart/form-data">
       <input type="hidden" name="code" value="<?php print $pro_code; ?>">
+      <input type="hidden" name="gazou_name_old" value="<?php print $pro_gazou_name_ord; ?>">
       商品名<br/>
       <!-- 名前を入れた状態で表示させる -->
       <input type="text" name="name" style="width:200px" value="<?php print $pro_name; ?>"><br/>
       価格<br/>
       <input type="text" name="price" style="width:50px" value="<?php print $pro_price; ?>">円<br/>
+      <br/>
+      <?php print $disp_gazou; ?>
+      <br/>
+      画像を選んでください。<br/>
+      <input type="file" name="gazou" style="width:400px"><br/>
       <br/>
       <input type="button" onclick="history.back()" value="戻る">
       <input type="submit" value="OK">
