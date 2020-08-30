4fc0d004f725fb1a2c1d7fd1a0858d332c28d859
sato0208 <kujira028@gmail.com>
2020-08-30 17:00:56 +0900

    [update]商品削除時に画像ファイルも一緒に削除

--- product/pro_delete.php
+++ product/pro_delete.php
@@ -23,7 +23,7 @@ error_reporting(E_ALL);
       $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
       // スタッフコードで絞り込む
-      $sql = 'SELECT name FROM mst_product WHERE code=?';
+      $sql = 'SELECT name,gazou FROM mst_product WHERE code=?';
       $stmt = $dbh ->prepare($sql);
       $data[]=$pro_code;
       $stmt->execute($data);
@@ -31,8 +31,15 @@ error_reporting(E_ALL);
       // スタッフ名を変数$pro_nameにコピー
       $rec = $stmt->fetch(PDO::FETCH_ASSOC);
       $pro_name=$rec['name'];
+      $pro_gazou_name=$rec['gazou'];
 
       $dbh = null;
+
+      if($pro_gazou_name == ''){
+        $disp_gazou='';
+      }else{
+        $disp_gazou='<img src="../gazou/'.$pro_gazou_name.'">';
+      }
     }
     catch(Exception $e){
       print 'ただいま障害により大変ご迷惑をおかけしております';
@@ -48,10 +55,14 @@ error_reporting(E_ALL);
     商品名<br/>
     <?php print $pro_name ?>
     <br/>
+    <?php print $disp_gazou ?>
+    <br/>
     この商品を削除していいですか<br/>
     <br/>
     <form method="post" action="pro_delete_done.php">
       <input type="hidden" name="code" value="<?php print $pro_code; ?>">
+      <!-- 次の画面で画像ファイルを削除するためにhiddenで画像ファイル名を渡す -->
+      <input type="hidden" name="gazou_name" value="<?php print $pro_gazou_name; ?>">
       <input type="button" onclick="history.back()" value="戻る">
       <input type="submit" value="OK">
     </form>
