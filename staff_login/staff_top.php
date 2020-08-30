d77c6deac2aa9c51c01027f2c333503124dba9d5
sato0208 <kujira028@gmail.com>
2020-08-30 18:24:21 +0900

    [fix]スタッフ管理画面へのリンク修正

--- staff_login/staff_top.php
+++ staff_login/staff_top.php
@@ -7,7 +7,7 @@
   <body>
     ショップ管理トップメニュー<br/>
     <br/>
-    <a href="../staff_list.php">スタッフ管理</a><br/>
+    <a href="../staff/staff_list.php">スタッフ管理</a><br/>
     <br/>
     <a href="../product/pro_list.php">商品管理</a><br/>
   </body>
