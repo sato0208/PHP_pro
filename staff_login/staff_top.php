c9e3a5f5077614d7dc62cfc665a684d91a8c433f
sato0208 <kujira028@gmail.com>
2020-09-21 11:14:05 +0900

    ログインしている人の名前を表示する

--- staff_login/staff_top.php
+++ staff_login/staff_top.php
@@ -7,6 +7,11 @@ if(isset($_SESSION['login'])==false)
   print '<a href="../staff_login/staff/login.html">ログイン画面へ</a>';
   exit();
 }
+else{
+  print $_SESSION['staff_name'];
+  print 'さんログイン中<br/>';
+  print '<br/>';
+}
 ?>
 
 <!DOCTYPE html>
